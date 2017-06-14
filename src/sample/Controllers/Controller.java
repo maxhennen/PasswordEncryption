817b0d6eeed636070e2a20b80d31d916859197c9
maxhennen <m.hennen@student.fontys.nl>
2017-06-14 22:26:12 +0200

    asdfg

---
+++ src/sample/Controllers/Controller.java
@@ -0,0 +1,11 @@
+package sample.Controllers;
+
+import javafx.scene.layout.AnchorPane;
+
+import javax.xml.soap.Node;
+import java.util.ArrayList;
+
+public abstract class Controller {
+    public abstract void setAnchorpane(AnchorPane anchorpane);
+    public abstract void setLayout();
+}
---
+++ src/sample/Controllers/LoginController.java
@@ -0,0 +1,15 @@
+package sample.Controllers;
+
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * Created by maxhe on 14-6-2017.
+ */
+public class LoginController extends Controller {
+    private AnchorPane Anchorpane;
+    public void setAnchorpane(AnchorPane anchorpane){Anchorpane = anchorpane;}
+
+    public void setLayout(){
+
+    }
+}
